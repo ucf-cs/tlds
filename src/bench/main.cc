8ba4d33687103b1d3b89b32c015bed38ae105242
Pierre LaBorde <pierrelaborde@knights.ucf.edu>
2016-11-11 11:54:25 -0500

    datafiles, script, fix tester?

--- src/bench/main.cc
+++ src/bench/main.cc
@@ -251,7 +251,7 @@ int main(int argc, const char *argv[])
 
     printf("Start testing %s with %d threads %d iterations %d txnsize %d unique keys %d%% insert %d%% delete %d%% update.\n", setName[setType], numThread, testSize, tranSize, keyRange, insertion, deletion, update);//(insertion + deletion) >= 100 ? 100 - insertion : deletion, update);
 
-    uint64_t numNodes = keyRange + (testSize*2);
+    uint64_t numNodes = testSize;//UINT64_C();
 
     switch(setType)
     {
